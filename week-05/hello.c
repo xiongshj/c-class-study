30aaf43fc8d4cdf2fc8eb315831f95c58b247a83
xiongshj <xiongshjcode@gmail.com>
2022-08-11 23:05:49 +0800

    完成第五周 5.1 节学习任务

---
+++ week-05/hello.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+
+ int main()
+ {
+    int n;
+
+    scanf("%d", &n);
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    
+    printf("%d!=%d\n", n, fact);
+
+    return 0;
+ }
